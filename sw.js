4f539e677f222895cb97e7eae2c6bd9bdff8811e
asarritzu-boop <a.sarritzu@gmail.com>
2026-03-29 13:36:33 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'convertitore-v1';
+const ASSETS = [
+  '/ConvertTerritori/',
+  '/ConvertTerritori/index.html',
+  '/ConvertTerritori/manifest.json',
+  '/ConvertTerritori/icona-192.png',
+  '/ConvertTerritori/icona-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
